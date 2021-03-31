b2c5a83ea010d369b99ece7abb7ede83fcf71174
Loknath <k20104661@kcl.ac.uk>
2021-03-31 12:37:47 +0100

    softmax back prop

--- cnn.py
+++ cnn.py
@@ -10,44 +10,67 @@ from maxpool import maxpool
 from softmax import softmax
 import tensorflow as tf
 import numpy as np
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-
-
-conv=conv3x3(8)
- # convolution helps in capturing patterns in image. 
- # a solbel filter can capture the edge in a image,smillary convolution will 
- # be capturing pattern in images
 
 
 
-mp=maxpool(2)
-# max pooling is layer where the image is reduced in dimention but mainly
-# in a image after convoling there is redudent pattrn in the nighbouring pixel
-# example when we find the edges in a image and consider a pixel its 
-# nighbouring pixel will contain just the edge and no other new information.
-# Hence by maxpooling we capture non redudent and unique patterns in the image
 
-sb=softmax(13*13*8,10) 
-# a softmax activation function generate a porbality distrubution for our prediction
-# using a softmax in our architecture node generates a probablity of a image to
-# belong to that node or that node that represents the class
-# another advantage is to use cross entrop loss for backpropogation of the network
+class cnn:
+    def __init__(self):
+        self.conv=conv3x3(8)
+        # convolution helps in capturing patterns in image.
+         # a solbel filter can capture the edge in a image,smillary convolution will 
+         # be capturing pattern in images
+        self.mp=maxpool(2)
+         # max pooling is layer where the image is reduced in dimention but mainly
+         # in a image after convoling there is redudent pattrn in the nighbouring pixel
+         # example when we find the edges in a image and consider a pixel its 
+         # nighbouring pixel will contain just the edge and no other new information.
+         # Hence by maxpooling we capture non redudent and unique patterns in the image
+        self.sb=softmax(13*13*8,10) 
+         # a softmax activation function generate a porbality distrubution for our prediction
+         # using a softmax in our architecture node generates a probablity of a image to
+         # belong to that node or that node that represents the class
+         # another advantage is to use cross entrop loss for backpropogation of the network
+        self.prep_data()
+        
+    def forwardpro(self,image,label):
+        layer1=self.conv.forward_pass(image)
+        layer2=self.mp.forward(layer1)
+        layer3=self.sb.forward(layer2)
+        loss=-np.log(layer3[label])
+        acc= 1 if np.argmax(layer3)==label else 0
+        return layer3,loss,acc
+    
+    def train(self,image,label,lr=.005):
+        gradiant=np.zeros(10)
+        # we need to calculate the gradiant of all the nodes
+        z,lo,ac=self.forwardpro(image,label)
+        gradiant[label]=-1/z[label]
+        gradiant=self.sb.backprop(gradiant,lr)
+        #print(lo,ac)
+        return lo,ac
 
-def forwardpro(image,label):
-    layer1=conv.forward_pass(image)
-    layer2=mp.forward(layer1)
-    layer3=sb.forward(layer2)
-    loss=-np.log(layer3[label])
-    acc= 1 if np.argmax(layer3)==label else 0
-    return layer3,loss,acc
+    def genisis(self):
+        loss = 0
+        num_correct = 0
+        for i, (im, label) in enumerate(zip(self.x_train, self.y_train)):
+            if i % 100 == 99:
+                print('[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %(i + 1, loss / 100, num_correct))
+                loss = 0
+                num_correct = 0
+            l, acc = self.train(im, label)
+            loss += l
+            num_correct += acc
+        
+    def prep_data(self):
+        (self.x_train, self.y_train), (self.x_test, self.y_test) = tf.keras.datasets.mnist.load_data()
 
-loss=0
-acc=0
-for i, (image,labe) in enumerate(zip(x_train[:10],y_train[:10])):
-    layer,lo,ac=forwardpro(image,labe)
-    loss+=lo
-    acc+=ac
-    print(acc)
+        
+    
+if __name__=="__main__":
+    cnn=cnn()
+    cnn.genisis()
+    
     
     
     
--- softmax.py
+++ softmax.py
@@ -15,18 +15,48 @@ class softmax:
         
         
     def forward(self,image):
+        self.last_input_shape=image.shape
         
         image=image.flatten()
-        # the network needs it flatten and not in its shape
+        self.last_input=image
+        # the network needs the image to be flatten and not in its orgi shape
         # dot operation of a matrix is smilar to what happens after flattening 
         # flattening involves converting rows as colum and concatinati it with
         # new row 
         # equation of softmax is e^of class/sum(e^(all class))
-        sum=np.dot(image,self.weights)+self.bias#
-        print(sum)
+        sum=np.dot(image,self.weights)+self.bias
+        #print(sum)
+        self.last_sum=sum
         exp=np.exp(sum)
         denominator=np.sum(exp,axis=0)
         return exp/denominator
+    
+    def backprop(self,dl_dsum,learn_rate):
+        # This method implements the deriavtive of the softbox and update rule for weight and bias
+        
+        for i ,gradiant in enumerate(dl_dsum):
+            if gradiant == 0:
+                continue
+            
+            t_exp=np.exp(self.last_sum)
+            S=np.sum(t_exp)
+            dout_dt = -t_exp[i] * t_exp / (S ** 2)
+            dout_dt[i] = t_exp[i] * (S - t_exp[i]) / (S ** 2)  
+            
+            # Gradients of totals against weights/biases/input
+            dt_dw = self.last_input
+            dt_db = 1
+            dt_dinputs = self.weights
+            # Gradients of loss against totals#
+            dL_dt = gradiant * dout_dt
+            # Gradients of loss against weights/biases/input
+            dL_dw = dt_dw[np.newaxis].T @ dL_dt[np.newaxis]
+            dL_db = dL_dt * dt_db
+            dL_dinputs = dt_dinputs @ dL_dt
+            
+            self.weights -= learn_rate * dL_dw
+            self.bias -= learn_rate * dL_db
+            return dL_dinputs.reshape(self.last_input_shape)
         
         
         
